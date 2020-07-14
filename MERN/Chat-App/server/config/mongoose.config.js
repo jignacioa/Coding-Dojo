cff0f9463ad3030efd15ced21d90dfac34b706b0
Jesus Aguilar <jignacio.aglr@gmail.com>
2020-07-13 23:00:49 -0700

    Update to Chat App

--- MERN/Chat-App/server/config/mongoose.config.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/users', {
-    useNewUrlParser: true,
-    useFindAndModify: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-});
