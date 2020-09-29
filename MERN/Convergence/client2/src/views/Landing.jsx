aec1a1e6e135739216f25e3b2d09da7c81a5a55e
Jesus Aguilar <jignacio.aglr@gmail.com>
2020-09-29 02:12:47 -0700

    Convergence(Chat-App): Edited the landing page

--- MERN/Convergence/client2/src/views/Landing.jsx
+++ MERN/Convergence/client2/src/views/Landing.jsx
@@ -9,11 +9,14 @@ export default function Landing() {
     return (
         <>
         <div className="bar"></div>
+        <div className="name-and-chatbox">
+        <div className="chatbox-div">
+        <div className="chat-box piece">Let's converge!</div>
+        </div>
         <Header header="Convergence" classname="landing-convergence"/>
-        <div className="chat-box piece"></div>
-        <div className="chat-box piece"></div>
+        </div>
         <div className="bar-bottom"></div>
-        
+        <button>Enter  Chatroom</button>
         </>
 
     )
